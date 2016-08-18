06583c2437bb6b65e42b5fd73571dc7f08e057b1
Abatino <a.abate.86@gmail.com>
2016-08-18 12:44:32 +0200

    Roets modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
+  get 'static_pages#about'
 
-  get 'static_pages/contact'
+  get 'static_pages#contact'
 
-  get 'static_pages/index'
+  get 'static_pages#index'
 
-  get 'welcome/index'
+  get 'welcome#index'
   
   root 'static_pages#index'
 
